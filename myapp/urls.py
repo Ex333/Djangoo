680af5a6d1fe7272503250a4e6a6d2c10675ea9d
Ex333 <haldas.mateusz@gmail.com>
2026-02-09 21:10:30 +0100

    next one we have our products on the web site..

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path    
-from .views import home, contact, about
+from .views import home, contact, about, item_detail
 
 urlpatterns = [
     path('', home, name='home'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
+    path("item/<int:id>/", item_detail, name="item_detail"),
 ]
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 # Create your views here.
@@ -11,3 +11,7 @@ def contact(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def item_detail(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    return render(request, 'item_detail.html', {'item': item})
