50997f0386ca2fd671b92518c2f23a6ebaeaff08
Ex333 <haldas.mateusz@gmail.com>
2026-02-09 20:32:36 +0100

    Next commit new htmls and new functions

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
