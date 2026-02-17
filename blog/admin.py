7084ca499b34a28cbadba5fe93417506bd631cb8
Ex333 <haldas.mateusz@gmail.com>
2026-02-17 17:05:38 +0100

    Admin has been added

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Post   
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published', 'updated_at')
+    prepopulated_fields = {'slug': ('title',)}  # cel --> źródło slug będzie generowany na podstawie title
